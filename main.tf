aa618fc57ebfa35d3a4af85af063c2c5ec3cccaf
rishiagrawal2609 <rishiagrawal2609@gmail.com>
2025-01-03 18:35:17 +0530

    final commit

--- main.tf
+++ main.tf
@@ -16,8 +16,7 @@ data "aws_instance" "filtered_instances" {
 }
 
 locals {
-  cutoff_date_local = "2025-01-03T18:00:00Z"
-  #timeadd(timestamp(), "${-(var.cutoff_days * 86400)}s")
+  cutoff_date_local = timeadd(timestamp(), "${-(var.cutoff_days * 86400)}s")
 }
 
 resource "aws_ebs_snapshot" "snapshots" {
